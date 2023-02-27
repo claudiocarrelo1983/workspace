db89365843365529778f66eeec32468f78144232
Claudio <claudiocarrelo1983@gmail.com>
2023-02-27 22:59:17 +0000

    rrr

--- frontend/views/client/stats.php
+++ frontend/views/client/stats.php
@@ -1,8 +1,46 @@
 <?php
 
+use yiier\chartjs\ChartJs;
 /* @var $this yii\web\View */
 
 
 ?>
 
-status
+<div class="container">
+<?= ChartJs::widget([
+						'type' => 'line',
+						
+						'options' => [
+							'height' => 320,
+							'width' => 400,	
+							'legend' => [	
+								'position'=> 'bottom',
+								'display'=> 'false'		
+							],													
+						],										
+						'data' => [
+							'labels' => [
+									Yii::t('app','Fat'),
+									Yii::t('app','Carbs'),
+									Yii::t('app','Protein'),
+                                    Yii::t('app','Protein'),
+                                    Yii::t('app','Protein')
+								],
+							'datasets' => [
+								[
+								
+									'label'=> '# of Votes',
+									'data' => [65, 59, 80, 81, 56, 55, 40],
+									'colors'=> ['#e0440e', '#e6693e', '#ec8f6e']
+								]
+							]
+						],
+						'clientOptions' => [
+							'legend' => [
+								'display' => true,
+								'position' => 'bottom',
+							]
+						]
+
+					]);?>	
+</div>                    
