5dd5ed3212b58e76565caf92cfa662ea09fd197c
Claudio <claudiocarrelo1983@gmail.com>
2023-08-22 15:35:04 +0100

    All sorted

--- frontend/views/clients/index.php
+++ frontend/views/clients/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use frontend\models\Clients;
+use common\models\User;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
@@ -11,7 +11,7 @@ use yii\db\Query;
 /** @var app\Models\ClientsSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
-$this->title = 'Clients';
+$this->title = 'Clients List';
 $this->params['breadcrumbs'][] = $this->title;
 
 
@@ -24,6 +24,7 @@ $userArr = $query->select('*')
 ]) 
 ->one();
 
+
 ?>
 
 <!-- start page title -->
@@ -33,18 +34,16 @@ $userArr = $query->select('*')
             <h4 class="mb-sm-0 font-size-18 pb-4">
                 <?= Html::encode($this->title) ?>
             </h4>  
-
             <div class="page-title-right">
                 <ol class="breadcrumb m-0">
                     <li class="breadcrumb-item">
                         <a href="javascript: void(0);">
-                            Clients
+                            Clients 
                         </a>
                     </li>
                     <li class="breadcrumb-item active"><?= $this->title ?></li>
                 </ol>
             </div>
-
         </div>
     </div>
 </div>
@@ -54,44 +53,29 @@ $userArr = $query->select('*')
         <div class="card">
             <div class="card-body">
                 <div class="clients-index">
-                    <!-- end page title -->
-                    <h4 class="card-title">Buttons example</h4>
-                    <p class="card-title-desc">The Buttons extension for DataTables
-                        provides a common set of options, API methods and styling to display
-                        buttons on a page that will interact with a DataTable. The core library
-                        provides the based framework upon which plug-ins can built.
-                    </p>
+               
+                    <?php
+                        if(!empty($userArr)){                            
+                    ?>
                     <div class="mt-5 mb-5">
                         <h5>
+                         
                             <?= Yii::t('app','Your registration Url:') ?> 
                             <?= Html::a(                            
-                                Url::base('https').Url::toRoute(['client/registration-calendar', 'code' => $userArr['company_code_url']]), 
-                                Url::toRoute(['client/registration-calendar', 'code' => $userArr['company_code_url']]),
+                                Url::base('https').Url::toRoute(['/page', 'code' => $userArr['company_code_url']]), 
+                                Url::toRoute(['/page', 'code' => $userArr['company_code_url']]),
                                 [           
                                 'target' => '_blank'
                                 ]                             
                             ) ?>    
                         </h5>
                     </div>
+                    <?php                                
+                        }
+                    ?>
                 
                     <div>
-                        <div id="datatable-buttons_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer">
-                            <div class="row">
-                                <div class="col-sm-12 col-md-6"><div class="dt-buttons btn-group flex-wrap">     
-                                    <button class="btn btn-secondary buttons-excel buttons-html5" tabindex="0" aria-controls="datatable-buttons" type="button">
-                                        <span>Excel</span>
-                                    </button> 
-                                    <button class="btn btn-secondary buttons-pdf buttons-html5" tabindex="0" aria-controls="datatable-buttons" type="button">
-                                        <span>PDF</span>
-                                    </button>                        
-                                </div>
-                            </div>
-                        </div>
-                
-                    <p class="pt-3">
-                        <?= Html::a('Create Client', ['create'], ['class' => 'btn btn-success']) ?>
-                    </p>
-
+              
                     <?= $this->render('_search', ['model' => $searchModel]); ?>
 
                         <?= GridView::widget([
@@ -99,13 +83,23 @@ $userArr = $query->select('*')
                             //'filterModel' => $searchModel,
                             'columns' => [
                                 ['class' => 'yii\grid\SerialColumn'],
-                                'username',
-                                'first_name',
-                                'last_name',
+                                'title',
+                                'gender', 
                                 'username',
                                 'nif',
+                                'first_name',
+                                'last_name',   
+                                'gender',  
+                                'dob',                   
                                 'contact_number',
                                 'email:email',
+                                'active',
+                                'created_date',
+                                /*
+                                'address',
+                                'postcode',
+                                'location',
+                                */
                                 //'nif',
                                 //'first_name',
                                 //'last_name',
@@ -140,12 +134,14 @@ $userArr = $query->select('*')
                                 //'subscription',
                                 //'subscription_startingdate',
                                 //'created_date',
+                             
                                 [
                                     'class' => ActionColumn::className(),
-                                    'urlCreator' => function ($action, Clients $model, $key, $index, $column) {
+                                    'urlCreator' => function ($action, User $model, $key, $index, $column) {
                                         return Url::toRoute([$action, 'id' => $model->id]);
                                     }
                                 ],
+                                                        
                             ],
                         ]); ?>
                 </div>
